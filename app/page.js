9ebeee167060e38848fea41acf96c667c9a73de5
Fredrik Löfgren <fredrik.lofgren@adlibris.com>
2024-10-26 09:44:21 +0200

    feat: update logo

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="flex items-center w-screen h-screen flex-col">
       <div className="w-64 relative h-24 mt-6">
-        <Image src="/images/logga-vit.svg" alt="sodertorn" layout={"fill"} objectFit={"contain"} />
+        <Image src="/images/cf-torn.png" alt="sodertorn" layout={"fill"} objectFit={"contain"} />
       </div>
 
       <div className="flex justify-center items-center w-full h-full flex-col">
